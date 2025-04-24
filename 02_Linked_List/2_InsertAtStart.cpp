b40b38fc51c1115dad941821a0a285c9dd984e5a
tesu123 <abhijit.rabidas.mca@gmail.com>
2025-04-24 09:53:05 +0530

    Update 2_InsertAtStart.cpp

--- 02_Linked_List/2_InsertAtStart.cpp
+++ 02_Linked_List/2_InsertAtStart.cpp
@@ -1,46 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-class Node
-{
-public:
+typedef struct Node {
     int data;
-    Node *next;
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
+    struct Node* next;
+} Node;
+
+Node* createNode(int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
-};
-
-void insertAtStart(Node *&head, int val)
-{
-
-    Node *newNode = new Node(val); // Create a new node with the given value
-    newNode->next = head;          // Link the new node to the current head
-    head = newNode;                // Update the head to point to the new node
+    newNode->data = val;
+    newNode->next = NULL;
+    return newNode;
 }
 
-int main()
-{
-    Node *head = new Node(1);
-    Node *newNode = new Node(2);
+void insertAtStart(Node** head, int val) {
+    Node* newNode = createNode(val);  // Create a new node with the given value
+    newNode->next = *head;            // Link the new node to the current head
+    *head = newNode;                 // Update the head to point to the new node
+}
+
+int main() {
+    // Create the initial linked list: 1->2->3
+    Node* head = createNode(1);
+    Node* newNode = createNode(2);
     head->next = newNode;
 
-    Node *newNode2 = new Node(3);
+    Node* newNode2 = createNode(3);
     newNode->next = newNode2;
 
     // Insert a new node at the start of the linked list
-    insertAtStart(head, 4); // Insert 0 at the start
+    insertAtStart(&head, 4);  // Insert 4 at the start
 
-    // Print the linked list
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
+    // Print the linked list (now: 4->1->2->3)
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
         temp = temp->next;
     }
-    cout << endl;
+    printf("\n");
+
+    // Free the allocated memory
+    temp = head;
+    while (temp != NULL) {
+        Node* nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
 
     return 0;
 }
