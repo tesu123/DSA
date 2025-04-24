b2bb9ce26403898aeb99e5381d5e67cc4a4205aa
tesu123 <abhijit.rabidas.mca@gmail.com>
2025-04-24 09:58:36 +0530

    Update 6_DeletionAtLast.cpp

--- 02_Linked_List/6_DeletionAtLast.cpp
+++ 02_Linked_List/6_DeletionAtLast.cpp
@@ -1,57 +1,66 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-class Node
-{
-public:
+typedef struct Node {
     int data;
-    Node *next;
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
-    }
-};
+    struct Node* next;
+} Node;
 
-void deleteAtLast(Node *&head)
-{
-    if (head == NULL) // If the list is empty, do nothing
-        return;
-    if (head->next == NULL) // If there's only one node, delete it
-    {
-        delete head;
-        head = NULL;
-        return;
+Node* createNode(int val) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
-    Node *temp = head;
-    while (temp->next->next != NULL) // Traverse to the second last node
-    {
-        temp = temp->next;
-    }
-    delete temp->next; // Delete the last node
-    temp->next = NULL; // Set the next of the second last node to NULL
+    newNode->data = val;
+    newNode->next = NULL;
+    return newNode;
 }
 
-int main()
-{
-    Node *head = new Node(1);
-    Node *newNode = new Node(2);
-    Node *newNode2 = new Node(3);
+void deleteAtLast(Node** head) {
+    if (*head == NULL) {               // If the list is empty, do nothing
+        return;
+    }
+    if ((*head)->next == NULL) {       // If there's only one node
+        free(*head);                   // Free the single node
+        *head = NULL;                  // Set head to NULL
+        return;
+    }
+    Node* temp = *head;
+    while (temp->next->next != NULL) { // Traverse to the second last node
+        temp = temp->next;
+    }
+    free(temp->next);                  // Free the last node
+    temp->next = NULL;                 // Set the next of second last node to NULL
+}
+
+int main() {
+    // Create the initial linked list: 1->2->3
+    Node* head = createNode(1);
+    Node* newNode = createNode(2);
+    Node* newNode2 = createNode(3);
 
     head->next = newNode;
     newNode->next = newNode2;
 
-    // Delete the first node of the linked list
-    deleteAtLast(head); // Delete the first node
+    // Delete the last node of the linked list
+    deleteAtLast(&head);  // Delete the last node (3)
 
-    // Print the linked list
-    Node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << " ";
+    // Print the linked list (now: 1->2)
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
         temp = temp->next;
     }
-    cout << endl;
+    printf("\n");
+
+    // Free the remaining allocated memory
+    temp = head;
+    while (temp != NULL) {
+        Node* nextNode = temp->next;
+        free(temp);
+        temp = nextNode;
+    }
 
     return 0;
 }
